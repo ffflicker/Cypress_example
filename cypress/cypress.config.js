7435c174f4b3c4a83f81defe117c9cb014226d86
ffflicker <matanyamot@gmail.com>
2024-12-07 23:04:04 +0300

    Исправил регситр структуры проекта / Доработал cypress.config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,11 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'wkzney',
   e2e: {
-    supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/e2e',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
 });
-const { defineConfig } = require("cypress");
