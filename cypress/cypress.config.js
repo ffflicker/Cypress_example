155f48e7e178978cb1152216b2c6ddd9aeb03e80
ffflicker <matanyamot@gmail.com>
2024-12-07 11:28:59 +0300

    Исправлены конфликты cypress.config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'wkzney',
   e2e: {
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+const { defineConfig } = require("cypress");
